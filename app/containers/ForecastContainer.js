bcdc9ecc9e25488a093861b93469a3af6982d661
Charles Javelona <charlesjavelona@gmail.com>
2016-05-03 18:09:22 -0400

    Changed configurations to support ES6

--- app/containers/ForecastContainer.js
+++ app/containers/ForecastContainer.js
@@ -1,49 +1,52 @@
-var React = require('react');
-var Forecast = require('../components/Forecast');
-var getForcast = require('../helpers/api').getForcast;
+import React, { Component } from 'react'
+import Forecast from '../components/Forecast'
+import getForcast from '../helpers/api'
 
-var ForecastContainer = React.createClass({
-  contextTypes: {
-    router: React.PropTypes.object.isRequired
-  },
-  getInitialState: function () {
-    return {
+class ForecastContainer extends Component {
+  constructor () {
+    super()
+    this.state = {
       isLoading: true,
       forecastData: {}
     }
-  },
-  componentDidMount: function () {
+  }
+  componentDidMount () {
     this.makeRequest(this.props.routeParams.city)
-  },
-  componentWillReceiveProps: function (nextProps) {
+  }
+  componentWillReceiveProps (nextProps) {
     this.makeRequest(nextProps.routeParams.city)
-  },
-  makeRequest: function (city) {
+  }
+  makeRequest (city) {
     getForcast(city)
-      .then(function (forecastData) {
+      .then((forecastData) => {
         this.setState({
           isLoading: false,
-          forecastData: forecastData
+          forecastData
         });
-      }.bind(this));
-  },
-  handleClick: function (weather) {
+      });
+  }
+  handleClick (weather) {
     this.context.router.push({
       pathname: '/detail/' + this.props.routeParams.city,
       state: {
-        weather: weather
+        weather
       }
     })
-  },
-  render: function () {
+  }
+  render () {
     return (
       <Forecast
         city={this.props.routeParams.city}
         isLoading={this.state.isLoading}
-        handleClick={this.handleClick}
-        forecastData={this.state.forecastData} />
+        handleClick={(weather) => this.handleClick(weather)}
+        forecastData={this.state.forecastData} 
+      />
     )
   }
-});
+}
 
-module.exports = ForecastContainer;
+ForecastContainer.contextTypes = {
+  router: React.PropTypes.object.isRequired
+}
+
+export default ForecastContainer
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,5 @@
-const webpack = require('webpack')
-const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const HTMLWebpackPluginConfig = new HtmlWebpackPlugin({
+var HtmlWebpackPlugin = require('html-webpack-plugin')
+var HTMLWebpackPluginConfig = new HtmlWebpackPlugin({
   template: __dirname + '/app/index.html',
   filename: 'index.html',
   inject: 'body'
@@ -9,8 +7,8 @@ const HTMLWebpackPluginConfig = new HtmlWebpackPlugin({
 
 module.exports = {
   entry: [
-    'webpack-dev-server/client?http://0.0.0.0:3000', //WebpackDevServer host and port
-    'webpack/hot/only-dev-server', //"only" prevents reload on syntax errors
+   // 'webpack-dev-server/client?http://0.0.0.0:3000', //WebpackDevServer host and port
+   // 'webpack/hot/only-dev-server', //"only" prevents reload on syntax errors
     'babel-polyfill',
     './app/index.js'
   ],
@@ -20,18 +18,13 @@ module.exports = {
   },
   module: {
     loaders: [
-      { test: /\.jsx?$/, 
+      { test: /\.js?$/, 
         exclude: /node_modules/, 
-        loaders: ['react-hot', 'babel-loader'],
-      },
-      { test: /\.css$/,
-        loader: 'style-loader!css-loader'
-      },
+        loader: 'babel-loader'
+      }
     ],
   },
   plugins: [
-    HTMLWebpackPluginConfig,
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin()
+    HTMLWebpackPluginConfig
   ],
 }
